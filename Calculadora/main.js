38d3dc6e0b2f192a09a3d843bcf8027524c1041f
Lucas <99835648+BrDio20@users.noreply.github.com>
2022-04-07 20:42:46 -0300

    Update main.js

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,13 +1,13 @@
-const buttons = document.getElementsByClassName('table-button')
+/* const buttons = document.getElementsByClassName('table-button')
 for (const button of buttons) {
   console.log(button.innerText)
   const key = button.innerText // MC
-  if(/[0-9]/.test(key)) {
-    
+  if(/[0-9]/.test(key)) {    
   } else {
     button.onclick = {
       1: () => {
-        console.log(1)
+        const numero = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numero + num;
       },
       2: () => {
         console.log(2)
@@ -18,9 +18,30 @@ for (const button of buttons) {
       MC: () => {
         console.log('MC')
       },
-      CE: () => {
-  
+      CE: () => {  
       }
     }[key]
   }
+}
+*/
+
+function insert(num) {
+    const numero = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('visor').innerHTML = "";
+}
+
+function back() {
+    const back = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = back.substring(0, back.length - 1);
+}
+
+function calculate() {
+    const result = document.getElementById('visor').innerHTML;
+    if (visor) {
+        document.getElementById('visor').innerHTML = eval(result);
+    }
 }
