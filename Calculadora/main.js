5864b18d853a4559fe533be61acd77af9bd17ae7
Lucas <99835648+BrDio20@users.noreply.github.com>
2022-04-08 16:26:43 -0300

    Update main.js

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,47 +1,30 @@
-/* const buttons = document.getElementsByClassName('table-button')
+const visorElement = document.getElementById('visor')
+const buttons = document.getElementsByClassName('table-button')
+// 
+function insert(char = '') {
+  visorElement.innerText = visorElement.innerText + char
+}
+function clear() {
+  visorElement.innerText = ''
+}
+function backspace() {
+  visorElement.innerText = visorElement.innerText.slice(0, -1)
+}
+function resultCalculate() {
+  const result = eval(visorElement.innerText)
+  visorElement.innerText = result
+  return result
+}
+function divideOnePerX() {
+  visorElement.innerText = 1 / resultCalculate()
+}
+//
 for (const button of buttons) {
-  console.log(button.innerText)
-  const key = button.innerText // MC
-  if(/[0-9]/.test(key)) {    
-  } else {
-    button.onclick = {
-      1: () => {
-        const numero = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = numero + num;
-      },
-      2: () => {
-        console.log(2)
-      },
-      3: () => {
-        console.log(3)
-      },
-      MC: () => {
-        console.log('MC')
-      },
-      CE: () => {  
-      }
-    }[key]
-  }
-}
-*/
-
-function insert(num) {
-    const numero = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = numero + num;
-}
-
-function clean() {
-    document.getElementById('visor').innerHTML = "";
-}
-
-function back() {
-    const back = document.getElementById('visor').innerHTML;
-    document.getElementById('visor').innerHTML = back.substring(0, back.length - 1);
-}
-
-function calculate() {
-    const result = document.getElementById('visor').innerHTML;
-    if (visor) {
-        document.getElementById('visor').innerHTML = eval(result);
-    }
+  const key = button.innerText
+  button.onclick = {
+    '←': backspace,
+    C: clear,
+    '=': resultCalculate,
+    '1/x': divideOnePerX
+  }[key] || (() => insert(key))
 }
